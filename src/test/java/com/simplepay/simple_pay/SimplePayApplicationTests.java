409855052bdb4a7d5a99713639d41cd831fa40ab
jo0oy <rkfkrh07@naver.com>
2025-01-04 13:44:34 +0900

    [init] init project

---
+++ src/test/java/com/simplepay/simple_pay/SimplePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplepay.simple_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
