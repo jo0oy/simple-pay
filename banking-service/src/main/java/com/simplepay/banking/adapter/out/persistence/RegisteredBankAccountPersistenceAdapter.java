9cf3ab2211a81da31d788ceca78d846b5510bde5
jo0oy <rkfkrh07@naver.com>
2025-01-23 02:33:31 +0900

    [feat] 등록된 계좌 정보 조회 Port 구현
    RegisteredBankAccountPersistenceAdapter에 구현
    related to #3

--- banking-service/src/main/java/com/simplepay/banking/adapter/out/persistence/RegisteredBankAccountPersistenceAdapter.java
+++ banking-service/src/main/java/com/simplepay/banking/adapter/out/persistence/RegisteredBankAccountPersistenceAdapter.java
@@ -1,13 +1,15 @@
 package com.simplepay.banking.adapter.out.persistence;
 
 import com.simplepay.banking.domain.RegisteredBankAccount;
+import com.simplepay.banking.port.out.FindRegisteredBankAccountInfoPort;
 import com.simplepay.banking.port.out.RegisterBankAccountPort;
 import com.simplepay.common.annotation.PersistenceAdapter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @PersistenceAdapter
-public class RegisteredBankAccountPersistenceAdapter implements RegisterBankAccountPort {
+public class RegisteredBankAccountPersistenceAdapter
+    implements RegisterBankAccountPort, FindRegisteredBankAccountInfoPort {
 
     private final RegisteredBankAccountSpringDataRepository registeredBankAccountRepository;
     private final RegisteredBankAccountMapper registeredBankAccountMapper;
@@ -20,5 +22,13 @@ public class RegisteredBankAccountPersistenceAdapter implements RegisterBankAcco
         var registeredBankAccount = registeredBankAccountRepository.save(registeredBankAccountJpaEntity);
 
         return registeredBankAccountMapper.mapToDomainEntity(registeredBankAccount);
+    }
+
+    @Override
+    public RegisteredBankAccount findById(Long bankAccountId) {
+        var registeredBankAccountJpaEntity = registeredBankAccountRepository.findById(bankAccountId)
+            .orElseThrow(RuntimeException::new);
+
+        return registeredBankAccountMapper.mapToDomainEntity(registeredBankAccountJpaEntity);
     }
 }
