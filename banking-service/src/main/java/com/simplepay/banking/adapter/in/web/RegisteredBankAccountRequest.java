3b96215de39a26dcd30cc10191bc6823df9ee294
jo0oy <rkfkrh07@naver.com>
2025-01-22 18:27:36 +0900

    [feat] 계좌 등록 Request/Respose DTO 클래스 구현
    - RegisteredBankAccountRequest
    - RegisteredBankAccountInfoResponse
    related to #3

---
+++ banking-service/src/main/java/com/simplepay/banking/adapter/in/web/RegisteredBankAccountRequest.java
@@ -0,0 +1,12 @@
+package com.simplepay.banking.adapter.in.web;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RegisteredBankAccountRequest(
+    @NotNull Long memberId,
+    @NotBlank String bankName,
+    @NotBlank String bankAccountNumber,
+    @NotNull Boolean linkedStatusIsValid
+) {
+}
